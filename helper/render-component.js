cac414311d20621077207509e18f21d7b4d89279
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-19 15:05:37 +0300

    Fix error when component name was resolved into function. Small refactor

--- helper/render-component.js
+++ helper/render-component.js
@@ -1,6 +1,11 @@
 var each = Ember.$.each;
 var merge = Ember.merge;
 var get = Ember.get;
+
+// import from ember-htmlbars/system/lookup-helper
+// todo: find more correct way to import it
+var lookupHelper = Ember.__loader.require('ember-htmlbars/system/lookup-helper').default;
+
 /**
  * Render component by name
  * @param componentPath
@@ -8,65 +13,74 @@ var get = Ember.get;
  *
  * Examples:
  *
-    {{render-component 'image-component' src="" class="image"}}
-    {{render-component 'pluralize-component' count=ungrouped.content.meta.total single="Object"}}
-    {{#render-component 'btn-component' action="addSection"}}
-    {{#render-component componentName _param='btn-component' action="addSection"}}
+ {{render-component 'image-component' src="" class="image"}}
+ {{render-component 'pluralize-component' count=ungrouped.content.meta.total single="Object"}}
+ {{#render-component 'btn-component' action="addSection"}}
+ {{#render-component componentName _param='btn-component' action="addSection"}}
  or
-    {{#render-component 'btn-component' action="addSection"}}
-     Add section
-    {{/render-component}}
+ {{#render-component 'btn-component' action="addSection"}}
+ Add section
+ {{/render-component}}
  or even
-    {{#render-component 'render-component' _param='btn-component' action="addSection"}}
-      Add section
-    {{/render-component}}
-  You also can use ___params to define all properties via hash like
-  instead of
-    {{render-component 'pluralize-component' count=ungrouped.content.meta.total single="Object"}}
-  you can use
-    {{render-component 'pluralize-component' ___params=hash}}
-  and hash is
-    hash = { count:ungrouped.content.meta.total, single:"Object"}
+ {{#render-component 'render-component' _param='btn-component' action="addSection"}}
+ Add section
+ {{/render-component}}
+ You also can use ___params to define all properties via hash like
+ instead of
+ {{render-component 'pluralize-component' count=ungrouped.content.meta.total single="Object"}}
+ you can use
+ {{render-component 'pluralize-component' ___params=hash}}
+ and hash is
+ hash = { count:ungrouped.content.meta.total, single:"Object"}
 
-  For cases when we need pass into component not only attributes but param too
-  When we need this?
-  for example you want to render
-     {{componentName paramName someOption=someOptionValue}}
-  You can do
-     {{#render-component 'componentName' someOption=someOptionValue}}
-  BUT! You can't do ( you can't pass more than one argument into component )
-     {{#render-component 'componentName' paramName someOption=someOptionValue}}
-  so for such cases you can use '_param' ( at line param or at hash property )
-     {{#render-component 'componentName' _param=paramName someOption=someOptionValue}}
-  or
-     {{#render-component 'componentName' __params=paramsHash}}
-  where paramsHash is
-     paramsHash = { _param:paramName, someOption=someOptionValue }
+ For cases when we need pass into component not only attributes but param too
+ When we need this?
+ for example you want to render
+ {{componentName paramName someOption=someOptionValue}}
+ You can do
+ {{#render-component 'componentName' someOption=someOptionValue}}
+ BUT! You can't do ( you can't pass more than one argument into component)
+ {{#render-component 'componentName' paramName someOption=someOptionValue}}
+ so for such cases you can use '_param' ( at line param or at hash property)
+ {{#render-component 'componentName' _param=paramName someOption=someOptionValue}}
+ or
+ {{#render-component 'componentName' __params=paramsHash}}
+ where paramsHash is
+ paramsHash = { _param:paramName, someOption=someOptionValue }
  */
 export default function(componentPath, options) {
-  var view = options.data.view,
-    resolvePath = function(path) {
-      return view.getStream(path).value();
-    },
-    component = resolvePath(componentPath) || componentPath,
-    helper = Ember.Handlebars.resolveHelper(options.data.view.container, component),
-    hash = options.hash;
+  options.hashContexts = options.hashContexts || {};
+  options.hashTypes = options.hashTypes || {};
+
+  const view = options.data.view;
+  const resolvePath = function(path) {
+      return view.getStream ? view.getStream(path).value() : path;
+    };
+  const getHelper = function(container, helperName) {
+      return lookupHelper(helperName, view, Ember.Handlebars).helperFunction;
+    };
+  let component = resolvePath(componentPath) || componentPath;
+  if (typeof component !== 'string') {
+    component = componentPath;
+  }
+  const helper = getHelper(options.data.view.container, component);
+  let hash = options.hash;
 
   // Allow to pass params as hash-object
-  if('___params' in hash) {
-    var params = resolvePath(hash.___params);
+  if ('___params' in hash) {
+    let params = resolvePath(hash.___params);
     delete hash.___params;
     hash = merge(params, hash);
     options.hash = hash;
   }
 
   // Allow to specify which params should be resolved
-  each(hash, function(key, value) {
-    var newKey = key.replace('__', '');
-    if((key.indexOf('__') === 0)){
-      options.hash[newKey] = value;//resolvePath(value);
+  each(hash, (key, value) => {
+    let newKey = key.replace('__', '');
+    if ((key.indexOf('__') === 0)) {
+      options.hash[newKey] = view._getBindingForStream(value);// resolvePath(value);
       options.hashContexts[newKey] = get(view, 'controller');
-      options.hashTypes[newKey] = "ID";
+      options.hashTypes[newKey] = 'ID';
     }
   });
 
@@ -76,7 +90,7 @@ export default function(componentPath, options) {
   //    {{componentName paramName someOption=someOptionValue}}
   // You can do
   //    {{#render-component 'componentName' someOption=someOptionValue}}
-  // BUT! You can't do ( you can't pass more than one argument into component )
+  // BUT! You can't do ( you can't pass more than one argument into component)
   //    {{#render-component 'componentName' paramName someOption=someOptionValue}}
   // so for such cases you can use
   //    {{#render-component 'componentName' _param=paramName someOption=someOptionValue}}
@@ -84,11 +98,20 @@ export default function(componentPath, options) {
   //    {{#render-component 'componentName' __params=paramsHash}}
   // where paramsHash is
   //    paramsHash = { _param:paramName, someOption=someOptionValue }
-  if('_param' in hash) {
-    var param = resolvePath(hash._param);
+  let param;
+  if ('_param' in hash) {
+    param = view._getBindingForStream(hash._param).value() || hash._param;
+    if (!Ember.isArray(param)) {
+      param = [param];
+    }
     delete hash._param;
-    helper.call(this, param, options);
+  }
+
+  if (!helper) {
+    Ember.Logger.warn(`render-component can\'t find handler for "${component}"`);
+  } else if (param) {
+    helper.call(this, param, options, options, options); // this strange params for instanceHelper: function(newView, hash, options, env) { at ember.debug.js
   } else {
-    helper.call(this, options);
+    helper.call(this, [], hash, options, options, options);
   }
 }
